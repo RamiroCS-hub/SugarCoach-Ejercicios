3017ca076dc5d04fd77156e8090a8eaddb562b21
Rama <ramirocarnicersouble8@gmail.com>
2023-08-25 10:36:31 -0300

    Primer ejercicio funcionando

--- PrimerEjercicio/app/src/main/java/com/example/primerejercicio/MainActivity.kt
+++ PrimerEjercicio/app/src/main/java/com/example/primerejercicio/MainActivity.kt
@@ -7,5 +7,24 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        fizzBuzz()
+    }
+    private fun fizzBuzz(){
+        for (i in 1..100){
+            if(i%3 == 0 && i%5 == 0){
+                println("fizzBuzz")
+                continue
+            }
+            if( i%3 == 0){
+                println("fizz")
+                continue
+            }
+            if( i%5 == 0){
+                println("buzz")
+                continue
+            }
+            println(i)
+
+        }
     }
 }
