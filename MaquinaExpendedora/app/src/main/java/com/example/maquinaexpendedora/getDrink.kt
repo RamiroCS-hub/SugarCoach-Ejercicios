5efdefb017829faa93c3ab21d3e78c75b1180865
Rama <ramirocarnicersouble8@gmail.com>
2023-09-12 09:18:53 -0300

    View Binding agregado - faltan fragments

--- MaquinaExpendedora/app/src/main/java/com/example/maquinaexpendedora/getDrink.kt
+++ MaquinaExpendedora/app/src/main/java/com/example/maquinaexpendedora/getDrink.kt
@@ -1,27 +1,31 @@
 package com.example.maquinaexpendedora
 
 import android.annotation.SuppressLint
+import android.content.Intent.getIntent
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.example.maquinaexpendedora.databinding.ActivityMainBinding
+import com.example.maquinaexpendedora.databinding.GetDrinkBinding
 import java.text.DecimalFormat
-class GetDrink : AppCompatActivity() {
+class GetDrink :  Fragment(R.layout.get_drink){
+
     val decimalFormat = DecimalFormat("#.##")
     var imgCan: ImageView? = null
     var txtMoney: TextView? = null
     lateinit var userInfo: MutableMap<Double, String>
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.get_drink)
-        imgCan = findViewById(R.id.imgCan)
-        txtMoney = findViewById(R.id.txtMoneyChg)
-        userInfo = getIntent().getSerializableExtra("sold") as MutableMap<Double, String>
+    lateinit var binding: GetDrinkBinding
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = GetDrinkBinding.bind(view)
+        userInfo = getIntent("sold").getSerializableExtra("sold") as MutableMap<Double, String>
         showInfo()
     }
 
-    @SuppressLint("SetTextI18n")
     private fun showInfo(){
         for( info in userInfo){
             when(info.value){
@@ -31,12 +35,12 @@ class GetDrink : AppCompatActivity() {
                 "Sprite" -> changeImg(R.drawable.sprite_can)
                 else -> Log.i("OnShowImage", "No hay ninguna imagen")
             }
-            txtMoney?.setText("Usted compro una ${info.value}. " +
+            binding.txtMoneyChg.setText("Usted compro una ${info.value}. " +
                     "Su vuelto es: ${decimalFormat.format(info.key)}$")
         }
 
     }
     private fun changeImg(src: Int){
-        imgCan?.setImageResource(src)
+        binding.imgCan.setImageResource(src)
     }
 }
